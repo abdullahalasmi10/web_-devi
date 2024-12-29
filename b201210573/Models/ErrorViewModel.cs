fda584413ab565285c8b9b8e74e05f4ad61fcffd
7fdx5 <104269041+7fdx5@users.noreply.github.com>
2024-12-29 03:07:34 +0300

    Delete b201210573 directory

--- b201210573/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace B201210597.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
