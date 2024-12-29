71090c07a755c2a758f2572722a06143ac371376
7fdx5 <104269041+7fdx5@users.noreply.github.com>
2024-12-29 03:01:53 +0300

    Add files via upload

---
+++ Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace B201210597.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
